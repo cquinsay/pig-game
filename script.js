b4e82af9705d0b61866f842806e96508565e2e0b
Charity Quinsay <cquinsay@gmail.com>
2021-12-06 14:25:17 -0800

    created roll dice function for dice button

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const diceBtn = document.querySelector('.btn--roll');
+const dice = document.querySelector('.dice');
+const holdBtn = document.querySelector('.btn--hold');
+const newGameBtn = document.querySelector('.btn--new');
+const score1 = document.querySelector('#score--0');
+const score2 = document.querySelector('#score--1');
+
+const rollDice = function () {
+    let num = Math.floor((Math.random() * 6) + 1);
+    dice.setAttribute('src', `dice-${num}.png`);
+
+}
+
+
+diceBtn.addEventListener('click', rollDice);
