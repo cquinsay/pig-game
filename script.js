017fa9a14497269758f08d08169a6048c334a0f0
Charity Quinsay <cquinsay@gmail.com>
2021-12-08 16:29:36 -0800

    hid dice upon starting new game

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player1 = document.querySelector('.player--1');
+const player2 = document.querySelector('.player--2');
 const diceBtn = document.querySelector('.btn--roll');
 const dice = document.querySelector('.dice');
 const holdBtn = document.querySelector('.btn--hold');
@@ -9,18 +11,32 @@ const points2 = document.getElementById('current--2');
 const score1 = document.getElementById('score--1');
 const score2 = document.getElementById('score--2');
 
+
+
+
 const newGame = function () {
     points1.innerHTML = 0;
     points2.innerHTML = 0;
     score1.innerHTML = 0;
     score2.innerHTML = 0;
+    dice.classList.add('hidden');
+    player1.classList.add('player--active');
+    player2.classList.remove('player--active');
+
 }
 
 const rollDice = function () {
     let num = Math.floor((Math.random() * 6) + 1);
     dice.setAttribute('src', `dice-${num}.png`);
+    if (num === 1) {
+
+
+
+    }
+
 
 }
 
 
 diceBtn.addEventListener('click', rollDice);
+newGameBtn.addEventListener('click', newGame);
